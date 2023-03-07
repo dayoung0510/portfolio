eb9c5f0bb066b9403c1addc8466ec671c3b450fc
dayoung <dayoungkim9105@gmail.com>
2023-03-07 21:12:31 +0900

    favicon, font 적용

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -7,26 +7,35 @@ ${reset};
 
 /* fonts */
 @font-face {
-  font-family: "pret";
+  font-family: "Pret";
   src: url("https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.woff")
     format("woff");
   font-weight: 100;
   font-style: normal;
 }
 @font-face {
-  font-family: "pret";
+  font-family: "Pret";
   src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
     format("woff");
   font-weight: 400;
   font-style: normal;
 }
 @font-face {
-  font-family: "pret";
+  font-family: "Pret";
   src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Black.woff")
     format("woff");
   font-weight: 800;
   font-style: normal;
 }
+@font-face {
+  font-family: 'Dung';
+  src: url('src/assets/fonts/DungGeunMo.woff2') format('woff2'),
+    url('src/assets/fonts/DungGeunMo.woff') format('woff'),
+    url('src/assets/fonts/DungGeunMo.eot') format('eot');
+  font-weight: 400;
+  font-style: normal;
+}
+
 
 html,body{
   width:100%;
@@ -38,7 +47,7 @@ html,body{
   }
   body {
     font-size: 36px;
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    font-family: "Dung", "Pret", Arial, sans-serif;
 
     @media ${({ theme }) => theme.device.mobile} {
       font-size: 18px;
